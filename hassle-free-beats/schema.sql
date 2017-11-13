953fb9602a4d6f61b55aae1da34e215dae803d1d
Mario Hoyos <marioanorganism@Marios-MacBook-Pro.local>
2017-11-13 17:10:45 -0600

    set up database tables

--- hassle-free-beats/schema.sql
+++ hassle-free-beats/schema.sql
@@ -1,2 +1,9 @@
 create table users (id serial primary key, user_id varchar(40), email varchar(50));
 
+create table beats (beat_id serial primary key, mp3_url varchar(70), cover_url varchar(70), title varchar(40), artist_or_genre varchar(40));
+
+create table cart (beat integer references beats (beat_id), user_id integer references users (id));
+
+create table invoice (invoice_id serial primary key, invoice_date varchar(40), total float, customer_id integer references users (id)); 
+
+create table invoice_line (line_id serial primary key, invoice_reference_id integer references invoice (invoice_id), sold_beat integer references beats (beat_id), price float);
