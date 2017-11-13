a078b50833410dc956d67ab208837d79b2b9e152
Mario Hoyos <mariohoyos92@gmail.com>
2017-11-13 12:33:04 -0600

    got a successful redirect off of auth0

--- hassle-free-beats/src/components/App/App.js
+++ hassle-free-beats/src/components/App/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MusicPlayer from "react-responsive-music-player";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin() {
+    window.location.href = "http://localhost:3001/api/login";
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={() => this.handleLogin()}> Log In </button>
+        <MusicPlayer
+          playlist={[
+            {
+              url: "http://fakeurl.mp3",
+              cover: "path/to/jpg",
+              title: "Despacito",
+              artist: ["Luis Fonsi", "Daddy Yankee"]
+            }
+          ]}
+        />
       </div>
     );
   }
