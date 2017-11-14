f0155ee7909d892c7bcf35564afd04994b2def43
Mario Hoyos <mariohoyos92@gmail.com>
2017-11-14 11:20:51 -0600

    updated linter config

--- hassle-free-beats/src/index.js
+++ hassle-free-beats/src/index.js
@@ -1,26 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./components/App/App.jsx";
 import registerServiceWorker from "./registerServiceWorker";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import * as Colors from 'material-ui/styles/colors';
-import { fade } from 'material-ui/utils/colorManipulator'
 
-const muiTheme= getMuiTheme({
-    palette: {
-        "primary1Color": "#96031a",
-        "primary2Color": "#616161",
-        "primary3Color": "rgba(0, 0, 0, 0.87)",
-        "accent1Color": "#616161",
-        "pickerHeaderColor": "#ffffff",
-        "alternateTextColor": "rgba(255, 255, 255, 0.87)",
-        "clockCircleColor": "rgba(0, 0, 0, 0.26)",
-    }
-})
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: "#96031a",
+    primary2Color: "#616161",
+    primary3Color: "rgba(0, 0, 0, 0.87)",
+    accent1Color: "#616161",
+    pickerHeaderColor: "#ffffff",
+    alternateTextColor: "rgba(255, 255, 255, 0.87)",
+    clockCircleColor: "rgba(0, 0, 0, 0.26)"
+  }
+});
 
 ReactDOM.render(
   <BrowserRouter>
