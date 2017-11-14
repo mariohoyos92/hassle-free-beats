0007607d9857d2e1565062dbc1bd896450ac63aa
Mario Hoyos <marioanorganism@Marios-MacBook-Pro.local>
2017-11-14 16:33:13 -0600

    have a semblance of a landing page, with some semblance of responsiveness

---
+++ hassle-free-beats/src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+// IMPORT MODULES
+import { Link } from "react-router-dom";
+import FlatButton from "material-ui/FlatButton";
+
+// IMPORT CSS
+import "./Header.css";
+
+class Header extends Component {
+  // LIFESTYLE FUNCTIONS
+
+  // CUSTOM FUNCS
+
+  //RENDER
+  render() {
+    return (
+      <div className="header">
+        <div className="header-logo">
+          <Link to="/">
+            <img
+              className="header-image"
+              src="https://s3.us-east-2.amazonaws.com/hassle-free-beats-untagged-audio/fullsizeoutput_2.jpeg"
+              alt="logo"
+            />
+          </Link>
+        </div>
+        <div className="header-button">
+          <FlatButton label="Home" />
+          <FlatButton label="About Us" />
+          <FlatButton label="Dashboard" />
+          <FlatButton label="Contact" />
+        </div>
+      </div>
+    );
+  }
+}
+// MAPSTATE TO PROPS FOR REDUX
+// function mapStateToProps(state){
+//     return state
+// }
+
+//EXPORT COMPONENT
+export default Header;
+//REDUX
+// export default connect(mapStateToProps, outputActions)();
