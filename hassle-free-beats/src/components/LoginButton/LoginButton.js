dd30bec786fd54b396893c91ca4e36c9cf466df8
Mario Hoyos <mariohoyos92@gmail.com>
2017-11-30 11:42:29 -0600

    more troubleshooting

--- hassle-free-beats/src/components/LoginButton/LoginButton.js
+++ hassle-free-beats/src/components/LoginButton/LoginButton.js
@@ -24,7 +24,7 @@ class LoginButton extends Component {
 
   handleLogin() {
     if (!this.state.loggedIn) {
-      window.location.href = "https://www.hasslefreebeats.com/api/login";
+      this.props.history.push("/api/login");
     } else {
       axios
         .get("/api/logout")
