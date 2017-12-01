2218d531aa9ee895aed7b5b04344b2167ea32d1f
Mario Hoyos <mariohoyos92@gmail.com>
2017-12-01 16:33:54 -0600

    refactor: cleaning up server file

--- hassle-free-beats/server/index.js
+++ hassle-free-beats/server/index.js
@@ -16,6 +16,8 @@ const mailgun = require("mailgun-js")({
 
 // IMPORT CONTROLLERS
 const cartController = require("./controllers/cart_controller");
+const authController = require("./controllers/authorization");
+const emailController = require("./controllers/email");
 
 // CONFIG VARIABLES BELOW
 // const { secret } = require("../config").session;
@@ -26,7 +28,7 @@ const cartController = require("./controllers/cart_controller");
 const port = process.env.PORT || 3001;
 
 // BEGIN SERVER
-const app = express();
+const app = (module.exports = express());
 
 // SERVE FRONTEND
 app.use(express.static(`${__dirname}/../build`));
@@ -59,23 +61,7 @@ passport.use(
       clientSecret: process.env.AUTH0_CLIENTSECRET,
       callbackURL: "/api/login"
     },
-    function(accessToken, refreshToken, extraParams, profile, done) {
-      app
-        .get("db")
-        .getUserByUserId([profile._json.user_id])
-        .then(response => {
-          if (!response[0]) {
-            app
-              .get("db")
-              .createUserFromAuth([profile._json.user_id, profile._json.email])
-              .then(created => {
-                return done(null, created[0]);
-              });
-          } else {
-            return done(null, response[0]);
-          }
-        });
-    }
+    getOrCreatUser
   )
 );
 
@@ -94,47 +80,68 @@ app.get(
     successRedirect: "https://www.hasslefreebeats.com/dashboard"
   })
 );
-
-app.get("/api/logstatus", (req, res, next) => {
-  res.status(200).json(req.session);
-});
-
-app.get("/api/logout", (req, res, next) => {
-  req.session.destroy();
-  res.redirect(200, "/");
-});
+app.get("/api/logstatus", authController.checkLogStatus);
+app.get("/api/logout", authController.logout);
 
 // GET BEATS FOR MUSIC STORE
-app.get("/api/beats", (req, res, next) => {
+app.get("/api/beats", getPlaylistFromDb);
+
+// SEND CONTACT-ME FORM EMAIL
+app.post("/api/contact", emailController.contactForm);
+
+// USER DASHBOARD
+app.get("/api/pastpurchases", getPastPurchasesFromDb);
+
+// SUCCESSFUL PAYMENT PAGE
+app.get("/api/purchases", getNewPurchasedTracks);
+
+// CART
+app.get("/api/cart", cartController.get);
+app.post("/api/cart", cartController.add);
+app.delete("/api/cart/:title", cartController.delete);
+
+// CHECKOUT
+app.post("/api/charge", postSaleToDbAndRedirect);
+
+// CATCH-ALL TO SERVE FRONT END FILES
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
+
+// ---------------------------------------------------------------------------Functions That Call The Database---------------------------------------------------------------------------------------------------------
+
+function getOrCreatUser(accessToken, refreshToken, extraParams, profile, done) {
+  app
+    .get("db")
+    .getUserByUserId([profile._json.user_id])
+    .then(response => {
+      if (!response[0]) {
+        app
+          .get("db")
+          .createUserFromAuth([profile._json.user_id, profile._json.email])
+          .then(created => {
+            return done(null, created[0]);
+          });
+      } else {
+        return done(null, response[0]);
+      }
+    });
+}
+
+function getPlaylistFromDb(req, res, next) {
   app
     .get("db")
     .getPlaylist()
     .then(response => res.status(200).json(response))
     .catch(res.status(500));
-});
+}
 
-// SEND CONTACT-ME FORM EMAIL
-app.post("/api/contact", function(req, res) {
-  let data = {
-    from: `${req.body.name} <${req.body.email}>`,
-    to: "support@hasslefreebeats.com, support@hasslefreebeats.com",
-    subject: `${req.body.subject}`,
-    text: `${req.body.emailBody}`
-  };
-
-  mailgun.messages().send(data, function(error, body) {
-    console.log(body);
-    if (body.message === "Queued. Thank you.") {
-      res.status(200).json("message sent");
-    } else {
-      res.status(500).json(error);
-      console.log(error);
-    }
-  });
-});
-
-// USER DASHBOARD
-app.get("/api/pastpurchases", (req, res, next) => {
+function getPastPurchasesFromDb(req, res, next) {
   if (req.session.passport) {
     app
       .get("db")
@@ -153,10 +160,9 @@ app.get("/api/pastpurchases", (req, res, next) => {
   } else {
     res.status(200).json("User not logged in");
   }
-});
+}
 
-// SUCCESSFUL PAYMENT PAGE
-app.get("/api/purchases", (req, res, next) => {
+function getNewPurchasedTracks(req, res, next) {
   app
     .get("db")
     .getPurchasedTracks()
@@ -167,15 +173,9 @@ app.get("/api/purchases", (req, res, next) => {
       res.status(200).json(filter);
     })
     .catch(res.status(500));
-});
+}
 
-// CART
-app.get("/api/cart", cartController.get);
-app.post("/api/cart", cartController.add);
-app.delete("/api/cart/:title", cartController.delete);
-
-// CHECKOUT
-app.post("/api/charge", (req, res) => {
+function postSaleToDbAndRedirect(req, res) {
   stripe.charges.create(req.body, (stripeErr, stripeRes) => {
     if (stripeErr) {
       res.status(500).send({ error: stripeErr });
@@ -249,14 +249,4 @@ app.post("/api/charge", (req, res) => {
       }
     }
   });
-});
-
-// CATCH-ALL TO SERVE FRONT END FILES
-const path = require("path");
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/../build/index.html"));
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
-});
+}
